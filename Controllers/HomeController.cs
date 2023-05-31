c751ecf258469344e5c98864df371a0051f540bf
Sarvenaz Sinaei <sarvenaz.sinaei@student.nbi-handelsakademin.se>
2023-05-31 19:33:53 +0200

    Det finnas många error om details filen och jag addera en using.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -26,6 +26,11 @@ namespace Inlamning_Webbapp.Controllers
         {
             return View();
         }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
