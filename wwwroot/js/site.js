dcb0c5d5228624aad5adc220d2817810b4378430
Sarvenaz Sinaei <sarvenaz.sinaei@student.nbi-handelsakademin.se>
2023-06-05 01:36:56 +0200

    Dessa ändringar i about sidan.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,43 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var swiper = new Swiper(".course-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        540: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
+var swiper = new Swiper(".reviews-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        540: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
